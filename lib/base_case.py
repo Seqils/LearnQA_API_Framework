be88e8ebef7e8d2c41effe79cb56b589482fd3aa
Seqils <donkov1337@gmail.com>
2022-12-25 15:28:10 +0600

    Edit user data tests updated, base case updated with reg and login generation

--- lib/base_case.py
+++ lib/base_case.py
@@ -2,8 +2,10 @@ import json
 from datetime import datetime
 import random
 import string
-
 from requests import Response
+
+from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class BaseCase:
@@ -50,3 +52,18 @@ class BaseCase:
     def generate_string(self, length: int):
         gen_string = ''.join(random.choices(string.ascii_lowercase, k=length))
         return gen_string
+
+    def create_new_user_and_get_user_id(self):
+        data = self.prepare_registration_data()
+        response = MyRequests.post('/user', data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, 'id')
+        user_id = self.get_json_value(response, 'id')
+        return data, user_id
+
+    def login_and_get_token_and_cookie(self, login_data: dict):
+        response = MyRequests.post('/user/login', data=login_data)
+        Assertions.assert_code_status(response, 200)
+        auth_sid = self.get_cookie(response, 'auth_sid')
+        token = self.get_headers(response, 'x-csrf-token')
+        return auth_sid, token
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -5,19 +5,18 @@ from lib.my_requests import MyRequests
 
 class TestUserEdit(BaseCase):
     def setup_method(self):
-        self.url_reg = "/user/"
+        self.url_auth = "/user/"
         self.url_login = "/user/login"
 
     def test_edit_just_created_user(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = MyRequests.post(self.url_reg, data=register_data)
+        response1 = MyRequests.post(self.url_auth, data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
 
         email = register_data['email']
-        first_name = register_data['firstName']
         password = register_data['password']
         user_id = self.get_json_value(response1, 'id')
 
@@ -34,7 +33,7 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed name"
 
-        response3 = MyRequests.put(self.url_reg + str(user_id),
+        response3 = MyRequests.put(self.url_auth + str(user_id),
                                    headers={'x-csrf-token': token},
                                    cookies={'auth_sid': auth_sid},
                                    data={'firstName': new_name}
@@ -43,9 +42,124 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
         # GET
-        response4 = MyRequests.get(self.url_reg + str(user_id),
+        response4 = MyRequests.get(self.url_auth + str(user_id),
                                    headers={'x-csrf-token': token},
                                    cookies={'auth_sid': auth_sid},
                                    )
 
         Assertions.assert_json_value_by_name(response4, 'firstName', new_name, "Wrong first name after edit")
+
+    def test_not_auth_user_data_change(self):
+        #REG new
+        data = self.prepare_registration_data()
+        response = MyRequests.post(self.url_auth, data=data)
+        user_id = self.get_json_value(response, "id")
+
+        #Trying to put as not auth
+        response2 = MyRequests.put(self.url_auth + str(user_id))
+        Assertions.assert_code_status(response2, 400)
+        Assertions.assert_content(response2, "Auth token not supplied")
+
+    def test_auth_user_can_change_data(self):
+        changing_data = {
+            'firstName': 'Guido',
+            'lastName': "van Rossum"
+        }
+        #REG new
+        data = self.prepare_registration_data()
+        response = MyRequests.post(self.url_auth, data=data)
+        user_id = self.get_json_value(response, "id")
+        email = data['email']
+        password = data['password']
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        #Login ang get auth_sid and token
+        response_log = MyRequests.post(self.url_login, data=login_data)
+        Assertions.assert_code_status(response_log, 200)
+        auth_sid = self.get_cookie(response_log, "auth_sid")
+        token = self.get_headers(response_log, "x-csrf-token")
+
+        #Trying to put as not auth
+        response_edit = MyRequests.put(self.url_auth + str(user_id),
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       data=changing_data
+                                       )
+        Assertions.assert_code_status(response_edit, 200)
+
+        # #check data
+        # response_check = MyRequests.get(self.url_auth + str(user_id),
+        #                                 headers={'x-csrf-token': token},
+        #                                 cookies={'auth_sid': auth_sid},
+        #                                 )
+        #
+        # Assertions.assert_json_value_by_name(
+        #     response_check,
+        #     'firstName',
+        #     changing_data['firstName'],
+        #     f"firstName wasn't edited"
+        # )
+        #
+        # Assertions.assert_json_value_by_name(
+        #     response_check,
+        #     'lastName',
+        #     changing_data['lastName'],
+        #     f"lastName wasn't edited"
+        # )
+
+    def test_auth_user_change_wrong_email(self):
+        changing_data = {
+            'email': 'wrongemail.lol.com',
+        }
+        # REG new
+        data, user_id = self.create_new_user_and_get_user_id()
+        email = data['email']
+        password = data['password']
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        # Login ang get auth_sid and token
+        auth_sid, token = self.login_and_get_token_and_cookie(login_data=login_data)
+
+        # Trying to put as not auth
+        response_edit = MyRequests.put(self.url_auth + str(user_id),
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       data=changing_data
+                                       )
+        Assertions.assert_code_status(response_edit, 400)
+        Assertions.assert_content(response_edit, "Invalid email format")
+
+    def test_auth_user_change_invalid_firstname(self):
+        changing_data = {
+            'firstName': 'q',
+        }
+        # REG new
+        data, user_id = self.create_new_user_and_get_user_id()
+        email = data['email']
+        password = data['password']
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        # Login ang get auth_sid and token
+        auth_sid, token = self.login_and_get_token_and_cookie(login_data=login_data)
+
+        # Trying to put as not auth
+        response_edit = MyRequests.put(self.url_auth + str(user_id),
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       data=changing_data
+                                       )
+        Assertions.assert_code_status(response_edit, 400)
+        Assertions.assert_json_value_by_name(response_edit,
+                                             'error',
+                                             'Too short value for field firstName',
+                                             'Something went wrong, perhaps firstName was changed'
+                                             )
